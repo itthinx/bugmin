a9561880aa3091f4ab8571fd1666f710f70f5a4f
itthinx <itthinx@gmail.com>
2013-08-20 14:31:35 +0200

    Create bugmin.php

---
+++ bugmin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * bugmin.php
+ *
+ * Copyright (c) www.itthinx.com
+ *
+ * This code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This header and all notices must be kept intact.
+ *
+ * @author Karim Rahimpur
+ * @package bugmin
+ * @since bugmin 1.0.0
+ *
+ * Plugin Name: Bugmin
+ * Plugin URI: http://www.itthinx.com/plugins/
+ * Description: Show stuff in admin sections for debugging purposes.
+ * Version: 1.0.0
+ * Author: itthinx
+ * Author URI: http://www.itthinx.com
+ * Donate-Link: http://www.itthinx.com
+ * License: GPLv3
+ */
+class Bugmin {
+}
+Bugmin::init();
